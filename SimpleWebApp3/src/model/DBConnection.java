5658b07472eb185a46c3b4803a7bc78fa446c204
Vladislav25702 <163073429+Vladislav25702@users.noreply.github.com>
2024-05-03 01:43:43 +0500

    Add files via upload

---
+++ SimpleWebApp3/src/model/DBConnection.java
@@ -0,0 +1,24 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/USERS1";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "password";
+
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+}
